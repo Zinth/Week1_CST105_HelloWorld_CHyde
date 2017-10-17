023b1989490fc7047937f6315c8739980f92ebfa
CHyde <blimple10@gmail.com>
2017-10-17 12:24:45 -0700

    First program for CST-105
    Prints Hello World to the console. This is proof of my repository setup.

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/** Program: Hello World Program
+* File: HelloWorld.java
+* Summary: Prints Hello World to the console.
+* Author: Chris Hyde
+* Date: October 17, 2017 
+**/
+
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("Hello World");
+		
+	}
+}
